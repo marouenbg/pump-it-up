bfd63828c8b0d431ba69a87783253b44f7d33695
marouenbg <marouen.b.guebila@gmail.com>
2018-03-24 15:01:12 +0100

    addded acceleration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Author: Tony DiCola
 # License: Public Domain
 import time
+import smbus
 
 # Import the ADXL345 module.
 import Adafruit_ADXL345
@@ -27,6 +28,10 @@ from chaco.chaco_plot_editor import ChacoPlotItem
 # Create an ADXL345 instance.
 accel = Adafruit_ADXL345.ADXL345()
 
+# Get I2C bus
+bus = smbus.SMBus(1)
+    
+    
 # Alternatively you can specify the device address and I2C bus with parameters:
 #accel = Adafruit_ADXL345.ADXL345(address=0x54, busnum=2)
 
@@ -143,8 +148,98 @@ class Controller(HasTraits):
         of our viewer object.
         """
         # Generate a new number and increment the tick count
-	x, y, z=accel.read()
-        new_val = x
+	#x, y, z=accel.read()
+   	
+        # ADXL345 address, 0x53(83)
+        
+        # Select bandwidth rate register, 0x2C(44)
+        
+        #		0x0A(10)	Normal mode, Output data rate = 100 Hz
+        
+        bus.write_byte_data(0x53, 0x2C, 0x0A)
+        
+        # ADXL345 address, 0x53(83)
+        
+        # Select power control register, 0x2D(45)
+        
+        #		0x08(08)	Auto Sleep disable
+        
+        bus.write_byte_data(0x53, 0x2D, 0x08)
+        
+        # ADXL345 address, 0x53(83)
+        
+        # Select data format register, 0x31(49)
+        
+        #		0x08(08)	Self test disabled, 4-wire interface
+        
+        #					Full resolution, Range = +/-2g
+        
+        bus.write_byte_data(0x53, 0x31, 0x08)
+        
+        #time.sleep(0.5)
+        
+        # ADXL345 address, 0x53(83)
+        
+        # Read data back from 0x32(50), 2 bytes
+        
+        # X-Axis LSB, X-Axis MSB
+        
+        data0 = bus.read_byte_data(0x53, 0x32)
+        
+        data1 = bus.read_byte_data(0x53, 0x33)
+        
+        # Convert the data to 10-bits
+        
+        xAccl = ((data1 & 0x03) * 256) + data0
+        
+        if xAccl > 511 :	
+        
+            xAccl -= 1024
+        
+        # ADXL345 address, 0x53(83)
+        
+        # Read data back from 0x34(52), 2 bytes
+        
+        # Y-Axis LSB, Y-Axis MSB
+        
+        data0 = bus.read_byte_data(0x53, 0x34)
+        
+        data1 = bus.read_byte_data(0x53, 0x35)
+        
+        # Convert the data to 10-bits
+        
+        yAccl = ((data1 & 0x03) * 256) + data0
+        
+        if yAccl > 511 :	
+        
+            yAccl -= 1024
+        
+        # ADXL345 address, 0x53(83)
+        
+        # Read data back from 0x36(54), 2 bytes
+        
+        # Z-Axis LSB, Z-Axis MSB
+        
+        data0 = bus.read_byte_data(0x53, 0x36)
+        
+        data1 = bus.read_byte_data(0x53, 0x37)
+        
+        # Convert the data to 10-bits
+        
+        zAccl = ((data1 & 0x03) * 256) + data0
+        
+        if zAccl > 511 :	
+        
+            zAccl -= 1024
+        
+        # Output data to screen
+        
+        #print "Acceleration in X-Axis : %d" %xAccl
+        
+        #print "Acceleration in Y-Axis : %d" %yAccl
+        
+        #print "Acceleration in Z-Axis : %d" %zAccl
+        new_val = xAccl
         self.num_ticks += 1
 
         # grab the existing data, truncate it, and append the new point.
@@ -212,3 +307,4 @@ popup=Demo()
 
 if __name__ == "__main__":
     popup.configure_traits()
+
