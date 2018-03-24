f14c11e0c814c7f1bc74749ff591abc8d0d2ee30
Unknown <aurelien.michot@gmail.com>
2018-03-24 20:08:24 +0100

    Frameset

--- aurel-ui.py
+++ aurel-ui.py
@@ -1,24 +1,11 @@
-from numpy import linspace, sin
-from traits.api import HasTraits, Str, Range, Float, Enum, Instance
-from traitsui.api import View, Group, Item, Label
-from chaco.api import Plot, ArrayPlotData
-from enable.api import ComponentEditor
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-class PumpIt(HasTraits):
-    name = Str
-    age = Range(1, 100)
-    weight = Float
-    gender = Enum('Male', 'Female')
+from tkinter import *
 
-    view = View(
-        VGroup(
-            Label('An Unthemed Label'),
-            Item('name'),
-            Item('age'),
-            Item('weight'),
-            Item('gender')
-        ),
-        title='Unthemed TraitsUI',
-    )
+fenetre = Tk()
 
-PumpIt().configure_traits()
+label = Label(fenetre, text="Hello World")
+label.pack()
+
+fenetre.mainloop()
