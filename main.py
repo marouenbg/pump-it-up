e840c2fc78b84e46fcb8c80fb8ebe7600aabaeb5
marouenbg <marouen.b.guebila@gmail.com>
2018-03-24 16:24:58 +0100

    added fft

--- main.py
+++ main.py
@@ -25,6 +25,22 @@ from pyface.timer.api import Timer
 from chaco.chaco_plot_editor import ChacoPlotItem
 
 
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
 # Create an ADXL345 instance.
 accel = Adafruit_ADXL345.ADXL345()
 
