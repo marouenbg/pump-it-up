d8eacffa75b2c131db6c9109bf078bab38e4378a
marouenbg <marouen.b.guebila@gmail.com>
2018-03-24 17:30:18 +0100

    added spectrum

--- spectrum.py
+++ spectrum.py
@@ -38,8 +38,8 @@ SPECTROGRAM_LENGTH = 100
 accel = Adafruit_ADXL345.ADXL345()
 def _create_plot_component(obj):
     # Setup the spectrum plot
-    frequencies = linspace(0.0, float(SAMPLING_RATE)/2, num=NUM_SAMPLES/2)
-    obj.spectrum_data = ArrayPlotData(frequency=frequencies)
+    #frequencies = linspace(0.0, float(SAMPLING_RATE)/2, num=NUM_SAMPLES/2)
+    #obj.spectrum_data = ArrayPlotData(frequency=frequencies)
     empty_amplitude = zeros(NUM_SAMPLES/2)
     obj.spectrum_data.set_data('amplitude', empty_amplitude)
 
@@ -48,9 +48,9 @@ def _create_plot_component(obj):
                            color="red")
     obj.spectrum_plot.padding = 50
     obj.spectrum_plot.title = "Spectrum"
-    spec_range = obj.spectrum_plot.plots.values()[0][0].value_mapper.range
-    spec_range.low = 0.0
-    spec_range.high = 5.0
+    #spec_range = obj.spectrum_plot.plots.values()[0][0].value_mapper.range
+    #spec_range.low = 0.0
+    #spec_range.high = 5.0
     obj.spectrum_plot.index_axis.title = 'Frequency (Hz)'
     obj.spectrum_plot.value_axis.title = 'Amplitude'
 
