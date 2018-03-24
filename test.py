aa7d6f41e7e2125cd6a6a8e4359457bb9079f956
marouenbg <marouen.b.guebila@gmail.com>
2018-03-24 09:44:57 +0100

    added spectrum examples

--- test.py
+++ test.py
@@ -11,6 +11,11 @@ by the user.
 
 # Major library imports
 import numpy as np
+from numpy import linspace, sin
+from traits.api import HasTraits, Instance
+from traitsui.api import Item, View
+from chaco.api import ArrayPlotData, HPlotContainer, Plot
+from enable.api import ComponentEditor
 
 # Enthought imports
 from traits.api import (Array, Callable, Enum, Float, HasTraits, Instance, Int,
@@ -153,5 +158,30 @@ class Demo(HasTraits):
 popup=Demo()
 
 
+#if __name__ == "__main__":
+#    popup.configure_traits()
+
+
+class ContainerExample(HasTraits):
+
+    plot = Instance(HPlotContainer)
+
+    traits_view = View(Item('plot', editor=ComponentEditor(), show_label=False),
+                       width=1000, height=600, resizable=True, title="Chaco Plot")
+
+    def _plot_default(self):
+        x = linspace(-14, 14, 100)
+        y = sin(x) * x**3
+        plotdata = ArrayPlotData(x=x, y=y)
+
+        scatter = Plot(plotdata)
+        scatter.plot(("x", "y"), type="scatter", color="blue")
+
+        line = Plot(plotdata)
+        line.plot(("x", "y"), type="line", color="blue")
+
+        container = HPlotContainer(Demo(), Demo())
+        return container
+
 if __name__ == "__main__":
-    popup.configure_traits()
+    ContainerExample().configure_traits()
